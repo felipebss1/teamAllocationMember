232e8978a69fc97d1db24e4b9a9d42323ffeefeb
Bruno Soares <bruno.sk.8@hotmail.com>
2022-09-03 12:02:20 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Employees from "./Employees";
+import Footer from "./Footer";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Employees />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
