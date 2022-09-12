0ecef52d97d4d662e52c33c0dfe5c0d9ec05b20c
Bruno Soares <bruno.sk.8@hotmail.com>
2022-09-12 16:53:13 -0300

    Fixed: key property

--- src/TeamMembers.jsx
+++ src/TeamMembers.jsx
@@ -3,6 +3,7 @@ import TeamMemberCard from "./TeamMemberCard";
 const TeamMembers = ({ employees, handleEmployeeCardClick, selectedTeam }) => {
   return employees.map((employee) => (
     <TeamMemberCard
+      key={employee.id}
       employee={employee}
       handleEmployeeCardClick={handleEmployeeCardClick}
       selectedTeam={selectedTeam}
